7fabf4e90961db36ee1496345b88f27c162d3341
BikramGupta <bikra2345@gmail.com>
2025-08-15 23:30:35 +0530

    First Commit

---
+++ Backend/index.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDb from './config/connectDB.js';
+import cookieParser from 'cookie-parser';
+import authRouter from './route/authRoute.js';
+import cors from "cors";
+dotenv.config()
+
+
+const port=process.env.PORT
+const app= express();
+
+
+//** middleware  */
+app.use(express.json())
+app.use(cookieParser())
+//** */
+
+
+//** Cors package which stabilize conaction between backend and frontend */
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
+//** */
+
+app.use("/api/auth",authRouter)
+
+app.get("/",(req,res)=>{
+    res.send("Hello from Server")
+})
+
+app.listen(port,()=>{
+    console.log(`Server is listening on port ${port}`)
+    connectDb()
+})
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import { ToastContainer } from 'react-toastify'
+
+export const serverUrl="http://localhost:8000"
+
+function App() {
+  return (
+    <>
+      <ToastContainer />
+      <Routes>
+        <Route path='/' element={<Home/>}  />
+        <Route path='/signup' element={<Signup/>}  />
+        <Route path='/login' element={<Login/>}  />
+      </Routes>
+    </>
+  )
+}
+
+export default App
